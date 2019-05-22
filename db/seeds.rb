2d3acd5e02f99e5782c0dc8a2250de6db2b927c9
Louis Paulet <louis.paulet.14@neoma-bs.com>
2019-05-22 17:36:33 +0200

    Rails Simple Airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flats_attributes = [
+  {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+  },
+  {
+  name: 'Light & Big Flat New-York',
+  address: '10 Clifton Gardens New York',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 120,
+  number_of_guests: 5
+  },
+  {
+  name: 'Light & Spacious Garden Flat Paris',
+  address: '10 Avenue des Champs Elysées Paris',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 130,
+  number_of_guests: 4
+  },
+  {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens Berlin',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 60,
+  number_of_guests: 8
+  }
+]
+Flat.create!(flats_attributes)
+puts 'Finished!'
